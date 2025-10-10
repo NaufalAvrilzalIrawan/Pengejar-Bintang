f7bc225c720d66b13adada27f257ab7132ddae73
Naufal Avrilzal Irawan <naufalavir@gmail.com>
2025-10-10 20:51:39 +0700

    Making the running and jumpping collision different

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,10 +8,14 @@ var has_barrier: bool = false
 #@onready var barrier = $Barrier
 var q_just_pressed = false
 var q_just_released := true
+@onready var collision_running: CollisionShape2D = $CollisionShapeRunning
+@onready var collision_jumping: CollisionShape2D = $CollisionShapeJumping
 
 func _physics_process(delta):
 	velocity.y += GRAVITY*delta
 	if is_on_floor():
+		collision_running.disabled = false
+		collision_jumping.disabled = true
 		if Input.is_action_pressed("jump"):
 			velocity.y = JUMP_SPEED
 			$JumpAudio.play()
@@ -19,5 +23,7 @@ func _physics_process(delta):
 		else:
 			$AnimatedSprite2D.play("Run")
 	else:
+		collision_running.disabled = true
+		collision_jumping.disabled = false
 		$AnimatedSprite2D.play("Float")
 	move_and_slide()
